c051f4aeb08ca3ecc05398a0a8753148cfa6ae02
Kiran Garimella <kiran@broadinstitute.org>
2025-07-24 13:43:11 -0700

    Add GNU parallel to docker image

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build -t lr-locityper:kvg_build_docker_locally -f containers/Dockerfile .
+# Build for AMD64 platform explicitly
+#docker build --platform linux/amd64 -t us.gcr.io/broad-dsp-lrma/lr-locityper:kvg_build_docker_locally -f containers/Dockerfile . && \
+docker build -t us.gcr.io/broad-dsp-lrma/lr-locityper:kvg_build_docker_locally -f containers/Dockerfile . && \
+    docker push us.gcr.io/broad-dsp-lrma/lr-locityper:kvg_build_docker_locally
